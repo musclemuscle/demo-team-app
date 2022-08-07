92bcf29975c9e2d5567783ed825d179abf96a18f
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2022-08-07 22:47:52 +0900

    firebaseと接続、firestoreを利用可能にした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
+import { addDoc, collection } from "firebase/firestore";
 import type { NextPage } from "next";
+import { ChangeEvent, useState } from "react";
 import styles from "src/pages/Home.module.css";
+import { db } from "src/utils/firebase";
 
 const Home: NextPage = () => {
-	return <div className={styles.container}>ここがトップページです。</div>;
+	const [text, setText] = useState("");
+
+	const setLatestText = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		setText(value);
+	};
+
+	const submitData = async () => {
+		try {
+			const docRef = await addDoc(collection(db, "users"), {
+				first: "Ada",
+				last: "Lovelace",
+				born: 1815,
+			});
+			console.log("Document written with ID: ", docRef.id);
+		} catch (e) {
+			console.error("Error adding document: ", e);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<input type="text" value={text} onChange={setLatestText} />
+			<button onClick={submitData}>Submit</button>
+		</div>
+	);
 };
 
 export default Home;
