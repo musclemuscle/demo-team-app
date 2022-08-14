ab73256b593bd7f9cb722f1501c78d6b30b20a23
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2022-08-15 07:59:43 +0900

    初期のディレクトリ構成を作成

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,16 @@
+/**
+ * @説明  : ログイン画面
+ *
+ * 以下のページに沿って作成してみてください。
+ * https://chuo-univ-app.web.app/LogInPage
+ */
+
+const Login: React.FC = () => {
+	return (
+		<div>
+			<p>Login</p>
+		</div>
+	);
+};
+
+export default Login;
