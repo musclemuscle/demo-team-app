8bcc2a2bf6368fd62ad301579a75e247d40a9df1
ryosukemochizuki <ryosukemochizuki.mochi@gmail.com>
2022-08-20 13:13:20 +0900

    未ログイン時のガードを作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import Authenticator from "src/components/functional/Authenticator";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	return (
 		<RecoilRoot>
-			<Component {...pageProps} />
+			<Authenticator>
+				<Component {...pageProps} />
+			</Authenticator>
 		</RecoilRoot>
 	);
 }
